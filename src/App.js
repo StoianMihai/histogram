a242e6157f99f59d1a06d5bbc0aef1f8a3f5814f
Stoian Mihai <mihai_stoian21@yahoo.com>
2021-03-16 15:01:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql,
+  useQuery,
+} from "@apollo/client";
+
+//conecting to API
+const client = new ApolloClient({
+  uri: "https://fakerql.stephix.uk/graphql",
+  cache: new InMemoryCache(),
+});
+
+function getPosts(count) {
+  return gql`
+    query GetPosts {
+      allPosts(count: ${count}) {
+        id
+        published
+        createdAt
+      }
+    }
+  `;
+}
+
+function Posts() {
+  const { loading, error, data } = useQuery(getPosts(100));
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return console.log(error);
+  return data.allPosts.map((post) => (
+    <div key={post.id} >
+      <p>{post.id}</p>
+      <p>{post.published && "Published"}</p>
+      <p>Created at:{post.createdAt}</p>
+      <hr />
+    </div >
+  ));
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+        <Posts />
+      </div>
+    </ApolloProvider>
   );
 }
 
